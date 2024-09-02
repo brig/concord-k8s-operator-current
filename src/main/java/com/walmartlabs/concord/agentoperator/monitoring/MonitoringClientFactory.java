12ec7fd946bd134856b349e82e59b4cb04a823c3
brig <ybrigo@gmail.com>
2024-09-02 18:40:36 -0400

    logs

--- src/main/java/com/walmartlabs/concord/agentoperator/monitoring/MonitoringClientFactory.java
+++ src/main/java/com/walmartlabs/concord/agentoperator/monitoring/MonitoringClientFactory.java
@@ -1,6 +1,11 @@
 package com.walmartlabs.concord.agentoperator.monitoring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MonitoringClientFactory {
+
+    private static final Logger log = LoggerFactory.getLogger(MonitoringClientFactory.class);
 
     public static MonitoringClient create(String baseUrl, String apiToken, String namespace) {
         String orgName = getEnv("ORG_NAME");
@@ -8,9 +13,11 @@ public class MonitoringClientFactory {
         String clusterAlias = getEnv("CLUSTER_ALIAS");
         String version = getEnv("VERSION");
 
-        if (orgName == null) {
+        if (orgName == null || storeName == null || clusterAlias == null || version == null) {
+            log.info("using NopMonitoringClient");
             return new NopMonitoringClient();
         } else {
+            log.info("using DefaultMonitoringClient");
             return new DefaultMonitoringClient(baseUrl, apiToken, namespace, orgName, storeName, clusterAlias, version);
         }
     }
